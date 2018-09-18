142ee9ce8c93f671b420bd950a415e340259c5f3
0lilauro <0lilauro7@gmail.com>
2018-09-18 07:14:38 -0300

    Apagado arquivos temporarios

--- test.php
+++
@@ -1,7 +0,0 @@
-
- 	<form  action="zipper.php" method="POST" enctype="multipart/form-data" >
- 		<input type="text" name="senha">
-
- 		<input type="file" name="imagem">
- 		<button type="submit">sub</button>
- 	</form>
